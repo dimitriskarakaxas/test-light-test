57d8f153d32835238a5fa2d8ec492b899e0fa13c
dimitriskarakaxas <karakaxasdimitrios@yahoo.com>
2021-10-29 15:05:09 +0300

    test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
-    MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID,
-    MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY,
-    MAILCHIMP_SERVER_PREFIX: process.env.MAILCHIMP_SERVER_PREFIX,
+    NEXT_PUBLIC_API_URL: "@next-public-api-url-production-pr7m",
+    NEXT_PUBLIC_URL: "@next-public-url-production-satj",
+    MAILCHIMP_LIST_ID: "@mailchimp-list-id-production-7c45",
+    MAILCHIMP_API_KEY: "@mailchimp-api-key-production-rdei",
+    MAILCHIMP_SERVER_PREFIX: "@mailchimp-server-prefix-production-ssdp",
   },
-}
+};
